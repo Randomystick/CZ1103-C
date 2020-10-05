89aa0d4e533ebad3c6b8b0771567e2d207d32048
Randomystick <bryankhleow@gmail.com>
2020-10-06 01:06:27 +0800

    Initial commit

---
+++ test/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%i\n", 49);
+    printf("%c", 49);
+    /*
+    char c1,c2,c3,c4,c5,c6;
+    scanf("%c%c%c%c",&c1,&c2,&c3,&c4);
+    c5=getchar();
+    c6=getchar();
+    putchar(c1);
+    putchar(c2);
+    printf("\n\n\n");
+    printf("c1 is %c\n",c1);
+    printf("c2 is %c\n",c2);
+    printf("c3 is %c\n",c3);
+    printf("c4 is %c\n",c4);
+    printf("c5 is %c\n",c5);
+    printf("c6 is %c\n",c6);
+*/
+}
