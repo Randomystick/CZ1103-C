06134b4c65d3328be920e8feb162658ae7f9661e
Randomystick <bryankhleow@gmail.com>
2020-10-07 21:11:42 +0800

    week8

--- week8/lab1.c
+++ week8/lab1.c
@@ -1,4 +1,4 @@
-///* QUESTION 1
+/* QUESTION 1
 // Write a C program that prints the ID and grade of each student in a class. The input
 // contains the student IDs and their marks. The range of the marks is from 0 to 100. The
 // relationships of the marks and grades are given below:
@@ -22,23 +22,193 @@ int main()
 
     do
     {
+        printf("Enter Student ID: ");
+        scanf("%d", &ID);
+        if (ID == -1)
+        {
+            break;
+        }
         printf("Enter Mark: ");
         scanf("%f", &mark);
+        if (mark == -1)
+        {
+            break;
+        }
         roundedMark = round(mark/10);
-        printf("%i", roundedMark);
+        //printf("%i", roundedMark);
+
+        printf("Grade = ");
+        switch (roundedMark)
+        {
+            case (10):
+            case (9):
+            case (8):
+                printf("A");
+                break;
+            case (7):
+                printf("B");
+                break;
+            case (6):
+                printf("C");
+                break;
+            case (5):
+                printf("D");
+                break;
+            case (4):
+            case (3):
+            case (2):
+            case (1):
+            case (0):
+                printf("F");
+                break;
+
+        }
+        printf("\n");
 
 
-        //printf("Enter Student ID: ");
-        //scanf("%d", &ID);
-    } while (mark != -1);
+    } while (mark != -1 || ID != -1);
+
+    return 0;
+}
+
+*/
+
+
+/* QUESTION 2
+// Write a C program that reads in several lines of non-negative integer numbers,
+// computes the average for each line and prints out the average. The value –1 in each line
+// of user input is used to indicate the end of input for that line.
+
+#include <stdio.h>
+
+int main()
+{
+    int noOfLines =0;
+    int nmbrValue =0;
+    int nmbrLength =0;
+    int sum =0;
+    float avg =0;
+    printf("Enter number of lines: ");
+    scanf("%i", &noOfLines);
+    fflush(stdin);
+
+    // Run the average calculator noOfLines times
+    for (int i = 1; i < noOfLines+1; i++)
+    {
+        //START FOR ONE LINE
+        // First (re)set sum and nmbrLength to 0
+        sum = nmbrLength = 0;
+
+        // Then prompt user for input
+        printf("Enter line %i (end with -1): ", i);
+
+        // For every element the user keyed in...
+        // do the math
+        // until -1 is reached
+        while (1==1)
+        {
+            scanf("%i", &nmbrValue);
+            if (nmbrValue == -1) { break; }
+            nmbrLength++;
+            //printf("number is %i\n", nmbrValue);
+
+            sum += nmbrValue;
+            //printf("sum is %i\n", sum);
+        }
+
+        // When -1 reached: print final avg value
+        avg = (float)sum / nmbrLength;
+        //printf("nmbrLength is %i", nmbrLength);
+        printf("Average = %f\n", avg);
+
+        // END FOR ONE LINE
+    }
+    return 0;
+}
+
+//*/
+
+
+/* QUESTION 3
+// Write a C program that accepts a positive number height between 1 and 10 as its
+// parameter value, and prints a triangular pattern according to height. Note that only 1, 2
+// and 3 are used to generate the patterns. A sample input and output session when the
+// program is called is given below. For example, pattern(2) will print the following pattern:
+// 1
+// 22
+// 333
+// while pattern(7) will print the following pattern:
+// 1
+// 22
+// 333
+// 1111
+// 22222
+// 333333
+// 1111111
+
+#include <stdio.h>
+
+int main()
+{
+    int height = 0;
+    int printer = 0;
+    printf("Enter the height: ");
+    scanf("%i", &height);
+    //printf("height is %i", height);
+
+    printf("Pattern: \n");
+    for (int i = 1; i < height+1; i++)
+    {
+        printer = i%3;
+        if (printer==0) { printer +=3; }
+        for (int j = 0; j<i; j++)
+        {
+            printf("%i",printer);
+        }
+        printf("\n");
+    }
+
 
     return 0;
 }
 
 //*/
 
-///* QUESTION 2
 
+/* QUESTION 4
+// Write a C program that computes the value of e^x
+// according to the following formula:
+// e^x = 1 + x/1! + x^2/2! + x^3/3! + ... + x^10/10!
 
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    const int POWER = 10;
+    float x;
+    float sum = 1;
+    int denom = 1;
+    printf("Enter x: ");
+    scanf("%f", &x);
+
+    for (int i = 1; i < POWER+1; i++)
+    {
+        //(Re)set denom to 1
+        denom = 1;
+
+        //Calculate denom as i*(i-1)*(i-2)...
+        for (int j = 1; j < i+1; j++)
+        {
+            denom = denom*j;
+        }
+
+        //printf("denom is %i", denom);
+        sum += pow(x,i)/denom;
+    }
+    printf("%.2f", sum);
+
+    return 0;
+}
 
 //*/
--- week8/tut1.c
+++ week8/tut1.c
@@ -1,1 +1,216 @@
+#include <stdio.h>
 
+int main()
+{
+/* QUESTION 1
+1a. char
+1b. int
+1c. float
+1d. string
+1e. hexadecimal
+1f. string
+1g. long
+1h. float
+1i. int
+1j. int
+1k. hexadecimal
+1l. hexadecimal
+*/
+
+
+/* QUESTION 2
+2a.
+AAAA
+
+2b.
+The integer 65 will be printed instead
+
+2c.
+)
+
+2d.
+e
+*/
+
+
+//int x,y;
+//scanf("%d/%d", &x, &y);
+//printf("%d %d", x, y);
+/* QUESTION 3
+3a.
+The contents in the input buffer will be assigned to x and y as digits separately, determined by whitespace delimiters.
+
+3b.
+Warning, then runtime error: scanf expects a pointer to a datatype (int in this case), but an int was supplied instead
+
+3c.
+The first digit in the input buffer will be assigned to x as an integer, while y will be unassigned
+*/
+
+
+//double A = 373737.0;
+//double B;
+//B = A * A * A + 0.37/A - A * A * A - 0.37/A;
+//printf("The value of B is %f.\n", B);
+/* QUESTION 4
+Floating point numbers such as double are inexact when stored in the machine,
+thus arithmetic operations involving multiple floating points will not be exact i.e. mathematically accurate.
+*/
+
+
+//int i=2;
+//int j=2;
+//int m=5;
+//int n=5;
+//float f=1.2;
+//float g=3.4;
+//printf("%i", m*j/j);
+//printf("%i", m/j*j);
+//printf("%i", (f+10)*20);
+//printf("%i", (i++)*n);
+//printf("%i", i++*n);
+//printf("%i", -12L*(g-f));
+//m = n = -- j;
+//printf("%i %i", m,n);
+//printf("%i", m*j/j);
+/* QUESTION 5
+5a.
+m * j / j
+= int * int / int
+= 5 * 2 / 2
+= 10 / 2
+= 5
+
+5b.
+m / j * j
+= int / int * int
+= 5 / 2 * 2
+= 2 * 2
+= 4
+
+5c.
+(f+10)*20
+= (float+int)*int
+= (float+float)*float
+= (1.2+10.0)*20.0
+= 11.2*20.0
+= 224.0
+
+5d.
+(i++)*n
+= (2++)*5
+= 10; then i = 3
+
+5e.
+i++ * n
+= 2++ * 5
+= 10; then i = 3
+
+5f.
+-12L * (g-f)
+= long * (float-float)
+= -12 * (2.2)
+= long * float
+= float * float
+= -12.0 * 2.2
+= -26.4
+
+5g.
+m = n = --j
+m = n = 1
+m and n = 1.
+
+5h.
+(int)g*10
+= int*int
+= 3*10
+= 30
+
+5i.
+(int)(g*10)
+= (int)(float*int)
+= (int)(float*float)
+= (int)(34.0)
+= 34
+
+5j.
+j = i+f
+int = int+float
+int = float+float
+int = 2.0+1.2
+int = 3.2
+j = 3.
+*/
+
+
+//const int number = 1;
+//int casee = 3;
+//#define SVALUE 1
+//switch (casee)
+//{
+//case(number*1):
+//    printf("xdd");
+//    break;
+//case(SVALUE*2):
+//    printf("lol");
+//    break;
+//case(3):
+//    printf("yeea");
+//    break;
+//}
+/* QUESTION 6
+6a.
+Valid, 76 is an integer.
+
+6b.
+Invalid, number is not a known value at compile time,
+hence number*2 does not evaluate to an integer or character
+constant at compile time.
+
+6c.
+Valid, SVALUE*2 is 20 which is an integer.
+
+6d.
+Invalid, 80.1 is a float and not an integer
+*/
+
+
+/* QUESTION 7 ?????????????????????
+7a.
+Define DLENGTH to be a smaller value
+
+7b.
+Nest the for-count loop in this other for loop:
+for (int i = 0; i < 1000; i++)
+{
+    ...
+    for (count = -DLENGTH; ...)
+    ...
+}
+
+7c.
+Change DLENGTH initiation from preprocessor directive to:
+int DLENGTH
+printf("Enter value of DLENGTH: );
+scanf("%i", DLENGTH);
+*/
+
+
+/* QUESTION 8 */
+//#include <stdio.h>
+//int main()
+//{
+    int x,y;
+    printf("x value is: ");
+    scanf("%i", &x);
+    printf("y value is: ");
+    scanf("%i", &y);
+    x = x+y; //now x is the sum of both numbers
+    y = x-y;
+    x = x-y;
+    //int temp = 0;
+    //temp = x;
+    //x = y;
+    //y = temp;
+    printf("Now x is %i, and y is %i", x, y);
+}
