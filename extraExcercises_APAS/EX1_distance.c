6185c50f56fa385dab631bd7c1b607d894923e63
Randomystick <bryankhleow@gmail.com>
2020-11-06 17:09:34 +0800

    extra excercises but most of it are already in the tut/lab lol

---
+++ extraExcercises_APAS/EX1_distance.c
@@ -0,0 +1,29 @@
+/*
+** The distance between two points (x1, y1) and (x2, y2) is given by sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)).
+** Write a C program that reads the (x, y) coordinates for two points, computes the distance between the two points.
+**/
+
+/*
+#include <stdio.h>
+#include <math.h>
+
+typedef struct
+{
+    int x,y;
+} coordinates;
+
+int main()
+{
+   // TODO
+   coordinates point1, point2;
+   printf("Enter first point x1 y1:\n");
+   scanf("%d %d", &point1.x, &point1.y);
+
+   printf("Enter second point x2 y2:\n");
+   scanf("%d %d", &point2.x, &point2.y);
+
+   printf("The distance is %.2f", sqrt(pow((point2.x-point1.x),2) + pow((point2.y-point1.y),2)));
+
+   return 0;
+}
+//*/
